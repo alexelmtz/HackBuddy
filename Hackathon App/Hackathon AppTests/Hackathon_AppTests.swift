e8a617c06235f2195454de3f6e5721acecf0c352
Alejandjro Elizondo <alexelmtz@outlook.com>
2016-08-27 12:31:24 -0500

    Created project

---
+++ Hackathon App/Hackathon AppTests/Hackathon_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hackathon_AppTests.swift
+//  Hackathon AppTests
+//
+//  Created by Alex Elizondo on 8/27/16.
+//  Copyright © 2016 Alex Elizondo. All rights reserved.
+//
+
+import XCTest
+@testable import Hackathon_App
+
+class Hackathon_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
